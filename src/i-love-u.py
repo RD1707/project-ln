13cf2f0b598ebe30efb6a2f2a071abfff22a5141
Ramon <souplemonpie@gmail.com>
2025-12-04 17:03:16 -0300

    Update i-love-u.py

--- src/i-love-u.py
+++ src/i-love-u.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-
 import sys
+import time
+import os
 
-
+# O desenho do coração original mantido
 __HEART__ = '''
 
           @@@@@@@@@@@                @@@@@@@@@@@
@@ -27,55 +28,93 @@ __HEART__ = '''
 
 '''
 
-
 class Color:
-    @property
-    def RED(self):
-        return '\x1b[0;31;40m'
+    # Cores para deixar o terminal bonito
+    RED = '\x1b[0;31;40m'
+    BOLD_YELLOW = '\x1b[1;33;40m'
+    CYAN = '\x1b[1;36;40m' # Adicionei uma cor nova (Ciano/Azul claro)
+    NORMAL = '\x1b[0m'
 
-    @property
-    def BOLD_YELLOW(self):
-        return '\x1b[1;33;40m'
+def limpar_tela():
+    # Comando para limpar a tela dependendo do sistema (Windows ou Linux/Mac)
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-    @property
-    def NORMAL(self):
-        return '\x1b[0m'
-
+def digitar_lento(texto, atraso=0.04):
+    """
+    Função que imprime o texto letra por letra para criar animação.
+    """
+    for letra in texto:
+        sys.stdout.write(letra)
+        sys.stdout.flush()
+        time.sleep(atraso)
+    print() # Pula uma linha no final
 
 class Valentine:
     def __init__(self, name):
         self.loved_one = name
 
     def romanticize(self):
+        # Lógica original para preencher o coração com o nome
         you_in_my_heart = __HEART__
+        
+        # Cria uma lista cíclica com as letras do nome
+        name_list = list(self.loved_one)
+        len_name = len(name_list)
+        count = 0
 
+        # Substitui cada @ por uma letra do nome
         while '@' in you_in_my_heart:
-            for letter in list(self.loved_one):
-                you_in_my_heart = \
-                    you_in_my_heart.replace('@', letter, 1)
+            you_in_my_heart = you_in_my_heart.replace('@', name_list[count % len_name], 1)
+            count += 1
 
         return you_in_my_heart
 
-    def i_love_you(self):
-        C = Color()
+    def show_love(self):
+        limpar_tela()
+        
+        # 1. Introdução com efeito de digitação
+        print(Color.CYAN)
+        digitar_lento(f"Olá, {self.loved_one}...", 0.1)
+        time.sleep(1)
+        digitar_lento("Tenho uma surpresa para ti.", 0.08)
+        time.sleep(1)
+        
+        # 2. Mostrar o coração colorido
+        cora_preenchido = self.romanticize()
+        print(Color.RED + cora_preenchido + Color.NORMAL)
+        time.sleep(0.5)
 
-        return '{}{}{}\n'\
-            'eu te amo muito, {}{}{}!'.format(
-                C.RED, self.romanticize(), C.NORMAL,
-                C.BOLD_YELLOW, self.loved_one, C.NORMAL
-            )
-
+        # 3. Mensagem Final
+        mensagem_final = f"Eu te amo muito, {Color.BOLD_YELLOW}{self.loved_one}{Color.NORMAL}!"
+        digitar_lento(mensagem_final, 0.05)
+        
+        # 4. Poema ou frases extras (Podes editar aqui!)
+        print("\n" + Color.CYAN)
+        frases = [
+            "Você é o meu código favorito.",
+            "Sem bugs, apenas amor.",
+            "Compilado com carinho para você."
+        ]
+        
+        for frase in frases:
+            time.sleep(1)
+            digitar_lento(" -> " + frase)
 
 def main():
+    # Tenta pegar o nome via argumento, senão pergunta
     try:
-        name = sys.argv[1]
-
-    except IndexError:
+        if len(sys.argv) > 1:
+            name = sys.argv[1]
+        else:
+            # Pergunta o nome se não for fornecido
+            limpar_tela()
+            name = input("Digite o nome da sua namorada: ")
+            if not name: name = "Meu Amor" # Nome padrão
+    except:
         name = 'Love'
 
     my_beloved = Valentine(name)
-    print(my_beloved.i_love_you())
-
+    my_beloved.show_love()
 
 if __name__ == '__main__':
     main()
