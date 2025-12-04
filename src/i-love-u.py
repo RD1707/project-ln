3b6f4052b9f0fe24d95c703be4606d54ddc9dd10
Ramon <souplemonpie@gmail.com>
2025-12-04 16:54:09 -0300

    a

--- src/i-love-u.py
+++ src/i-love-u.py
@@ -60,7 +60,7 @@ class Valentine:
         C = Color()
 
         return '{}{}{}\n'\
-            'I love you very much, {}{}{}. Happy valentine\'s day!'.format(
+            'eu te amo muito, {}{}{}!'.format(
                 C.RED, self.romanticize(), C.NORMAL,
                 C.BOLD_YELLOW, self.loved_one, C.NORMAL
             )
